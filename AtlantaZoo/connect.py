de7d04166a3881ed1b1968ed45498042adca53d0
tjtanaa <tunjian1996@gmail.com>
2018-11-20 16:24:17 -0500

    adminFunc done

--- AtlantaZoo/connect.py
+++ AtlantaZoo/connect.py
@@ -99,13 +99,13 @@ if __name__ == "__main__":
     state = 0
 
     # uncomment this if you need a visitor ID
-    loginIdentity = [('xavier_swenson', '34cc93ece0ba9e3f6f235d4af979b16c', 'xavierswenson@outlook.com', 'visitor')]    
+    # loginIdentity = [('xavier_swenson', '34cc93ece0ba9e3f6f235d4af979b16c', 'xavierswenson@outlook.com', 'visitor')]    
 
     # uncomment this if you need a staff ID
     # loginIdentity = [('martha_johnson', '7c6a180b36896a0a8c02787eeafb0e4c', 'marthajohnson@hotmail.com', 'staff')]
 
     # uncomment this if you need an admin ID
-    # loginIdentity = [('admin1', 'e3274be5c857fb42ab72d786e281b4b8', 'adminemail@mail.com', 'admin')]
+    loginIdentity = [('admin1', 'e3274be5c857fb42ab72d786e281b4b8', 'adminemail@mail.com', 'admin')]
 
     # DEBUGGING USE
     # display current user data
