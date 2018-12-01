f3ef077a00008e06629e63f07dc57f378f7df185
Ling Yiqi <vera@ipsec-143-215-26-20.vpn.gatech.edu>
2018-11-30 20:50:58 -0500

    update sql

---
+++ tests/MySQL/animalDetails.sql
@@ -0,0 +1,6 @@
+#display animal details
+SELECT Name, Species, Type, Age, Exhibit FROM ANIMAL  
+WHERE Name = 'Brad' AND Species = 'Bald Eagle';
+
+-- SELECT Name, Species, Type, Age, Exhibit FROM ANIMAL  
+-- WHERE Name = $Name AND Species = $Species ;
