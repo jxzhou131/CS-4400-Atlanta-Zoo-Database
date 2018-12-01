a4a8a2626e063b0d3f04322bf1406612ce893f9f
Chloe Chou <chloe@c-24-72-150-162.ni.gigamonster.net>
2018-11-30 20:47:02 -0500

    added sql

---
+++ tests/MySQL/adminAddAnimals.sql
@@ -0,0 +1,6 @@
+-- admin add animals
+-- INSERT INTO ANIMAL (Name, Species, Type, Age,  Exhibit)
+-- VALUES ($Name, $Species, $Type, $Age,  $Exhibit);
+
+INSERT INTO ANIMAL (Name, Species, Type, Age,  Exhibit)
+VALUES ("abc", "abc", "abc", 12,  "Jungle");
